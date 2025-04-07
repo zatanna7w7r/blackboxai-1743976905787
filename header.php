6ac2eb1a8932ce4af540aa50f6c830e8233b312f
zatanna <83142638+zatanna7w7r@users.noreply.github.com>
2025-04-06 23:02:45 -0600

    Add header.php 1744002164989

--- header.php
+++ header.php
@@ -12,16 +12,13 @@
                 <i class="fas fa-plus mr-2"></i>Crear Curso
             </a>
             
-            <?php if(isset($_SESSION['user_id'])): ?>
+            <?php if(isset($_SESSION['user'])): ?>
                 <div class="relative group">
                     <button class="flex items-center space-x-2 focus:outline-none">
                         <div class="w-10 h-10 rounded-full bg-white flex items-center justify-center text-purple-600 font-bold">
-                            <?php 
-                            $username = $_SESSION['username'];
-                            echo strtoupper(substr($username, 0, 1)); 
-                            ?>
+                            <?php echo strtoupper(substr($_SESSION['user']['username'], 0, 1)); ?>
                         </div>
-                        <span><?php echo htmlspecialchars($_SESSION['username']); ?></span>
+                        <span><?php echo htmlspecialchars($_SESSION['user']['username']); ?></span>
                     </button>
                     <div class="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-50 hidden group-hover:block">
                         <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-purple-100">Mi Perfil</a>
